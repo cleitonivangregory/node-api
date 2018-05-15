d0a4385bd8b60d9fcfc72e3ff39413d9f1bca373
Cleiton Ivan Gregory <gregory@gamatec.com.br>
2018-05-15 08:33:53 -0300

    teste

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 global.SALT_KEY = 'f5b99242-6504-4ca3-90f2-05e78e5761ef';
 global.EMAIL_TMPL = 'Olá, <strong>{0}</strong>, seja bem vindo à Node Store!';
 
-const urlBD = 'mongodb://127.0.0.1:27017/apinode';
+const urlBD = 'mongodb://gregory:gregory@ds119160.mlab.com:19160/node-store-app';
 
 module.exports = {
     connectionString: urlBD
